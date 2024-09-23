bde1718279e0cf062787eee6de3669b7e158231f
Ianm-SP <hcafe759@gmail.com>
2024-09-22 23:47:42 -0300

    first

---
+++ exercicio 01/exercicio 01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite o nome do vendedor:");
+string nomeVendedor = Console.ReadLine();
+
+Console.WriteLine("Digite o salário fixo do vendedor:");
+double salarioFixo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o total de vendas efetuadas pelo vendedor:");
+double totalVendas = double.Parse(Console.ReadLine());
+
+double comissao = totalVendas * 0.15;
+double totalReceber = salarioFixo + comissao;
+
+Console.WriteLine($"TOTAL = R$ {totalReceber:F2}");
---
+++ exercicio 03/exercicio 03/Program.cs
@@ -0,0 +1,25 @@
+﻿double pi = 3.14159;
+
+// Leitura dos valores A B C
+Console.WriteLine("Digite o valor de A:");
+double A = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de B:");
+double B = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de C:");
+double C = double.Parse(Console.ReadLine());
+
+// Cálculos das áreas
+double areaTriangulo = (A * C) / 2.0;
+double areaCirculo = pi * (C * C);
+double areaTrapezio = ((A + B) * C) / 2.0;
+double areaQuadrado = B * B;
+double areaRetangulo = A * B;
+
+// Exibição
+Console.WriteLine($"TRIANGULO: {areaTriangulo:F3}");
+Console.WriteLine($"CIRCULO: {areaCirculo:F3}");
+Console.WriteLine($"TRAPEZIO: {areaTrapezio:F3}");
+Console.WriteLine($"QUADRADO: {areaQuadrado:F3}");
+Console.WriteLine($"RETANGULO: {areaRetangulo:F3}");

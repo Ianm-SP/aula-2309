0d235d7387e599b5b1fb9fc00b9cce3dd83d2f66
Ianm-SP <iangof08@gmail.com>
2024-09-23 14:28:42 -0300

    Add files via upload

---
+++ exercicio 5/exercicio 5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("coloque x1");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("coloque y1");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("coloque x2");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("coloque y2");
+double y2 = double.Parse(Console.ReadLine());
+
+// Cálculo da distância usando a fórmula fornecida
+double distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+// Saída: exibir a distância com 4 casas decimais
+Console.WriteLine(distancia.ToString("F4"));
---
+++ exercicio 6/exercicio 6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("coloque o nome");
+string nome = Console.ReadLine();
+Console.WriteLine("coloque o peso");
+double peso = double.Parse(Console.ReadLine());
+Console.WriteLine("coloque a altura");
+double altura = double.Parse(Console.ReadLine());
+
+double imc = peso / Math.Pow(altura, 2);
+
+Console.WriteLine($"{nome}, seu IMC é {Math.Round(imc)}");
